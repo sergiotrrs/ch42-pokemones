89b4f7517f36622ac4199723b75cabaf3893e99b
Sergio Torres <sergio.torres@idr.edu.mx>
2024-07-02 12:22:22 -0600

    ci: length en fullname entre 3 y 50 caracteres

--- src/components/validateInputsForm/validateFullName.js
+++ src/components/validateInputsForm/validateFullName.js
@@ -4,9 +4,9 @@ const validateFullName = ( fullName ) => {
     let isValid = true;
 
     
-    if( fullName.length <= 3){ // Longitud mínima
+    if( fullName.length < 3 ){ // Longitud mínima
         isValid = false;
-    } else if (  fullName.length >= 50){ // Longitud máxima
+    } else if (  fullName.length > 50 ){ // Longitud máxima
         isValid = false;
     }
 
