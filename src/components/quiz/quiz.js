7def28877b5b2d62e6f43386a54a7aacc397c387
Sergio Torres <sergio.torres@idr.edu.mx>
2024-07-03 13:22:33 -0600

    ci: solución 1 de cantidadDeLetras()

--- src/components/quiz/quiz.js
+++ src/components/quiz/quiz.js
@@ -2,13 +2,21 @@
 // Calcular la cantidad de letras que hay
 const cadena = "parangaricutirimicuaro";
 
-const cantidadDeLetras = (letra, frase) => {
-    let letrasEncontradas = 0;
 
-    // TODO realizar la búsqueda de letras.
+const cantidadDeLetras = ( letra, frase ) => {
+    let letrasEncontradas = 0;
+    const letraEnMinuscula = letra.toLowerCase();
+    const fraseEnMinuscula = frase.toLowerCase();
+
+    for(let i=0; i < fraseEnMinuscula.length; i++  ){
+        if( letraEnMinuscula === fraseEnMinuscula[i] ) {  // letra === frase.charAt( i )
+           letrasEncontradas++; // letrasEncontradas = letrasEncontradas + 1; 
+        }
+    }
 
     return letrasEncontradas;
-}
+} 
+
 
 
 // exportacion nombrada
