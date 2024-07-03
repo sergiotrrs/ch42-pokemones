86415f3271158bf313863f1a6ea1896b52344f87
Sergio Torres <sergio.torres@idr.edu.mx>
2024-07-03 14:06:13 -0600

    add: funcion palindrome

---
+++ src/components/quiz/palindromo.js
@@ -0,0 +1,14 @@
+/**
+ Palíndromo: palabra que se lee igual de izquierda a derecha
+  y viceversa.
+
+  Ejemplo: oso, Anita lava la tina
+*/
+const palindrome = ( phrase ) =>{
+
+    // TODO realizar la funcionalidad
+
+    return false;
+}
+
+export { palindrome };
